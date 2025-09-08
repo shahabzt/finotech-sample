9e9649fa124cfa703b3388bbe133c055d0cc21c3
sh.banijamali <sh.banijamali@podro.com>
2025-09-08 21:45:20 +0330

    feat: install new packages for initial config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
+import { RouterProvider } from "react-router-dom";
+import "./App.css";
+import Router from "./pages/router";
+import createCache from "@emotion/cache";
+import rtlPlugin from "stylis-plugin-rtl";
+import { prefixer } from "stylis";
+import { CacheProvider } from "@emotion/react";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./styles/theme";
 
-import './App.css';
 
 function App() {
+  const cacheRtl = createCache({
+    key: "muirtl",
+    stylisPlugins: [prefixer, rtlPlugin],
+  });
+
   return (
-    <div className="App">
-   
-    </div>
+    <CacheProvider value={cacheRtl}>
+      <ThemeProvider theme={theme}>
+        {/* <Provider store={store}> */}
+        <RouterProvider router={Router} />
+        {/* </Provider> */}
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 

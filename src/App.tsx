7cbca86d44c60a2e956146013244a877f96467cc
sh.banijamali <sh.banijamali@podro.com>
2025-09-11 01:58:05 +0330

    fix: removed unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { RouterProvider } from "react-router-dom";
-import "./App.css";
 import Router from "./pages/router";
 import createCache from "@emotion/cache";
 import rtlPlugin from "stylis-plugin-rtl";

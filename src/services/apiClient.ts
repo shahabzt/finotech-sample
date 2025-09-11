a436c570c9b04920b29e9fe9abad50088abe5774
sh.banijamali <sh.banijamali@podro.com>
2025-09-11 18:15:14 +0330

    feat: add env file

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://jsonplaceholder.typicode.com",
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
     "Cache-Control": "no-cache",
     Pragma: "no-cache",
